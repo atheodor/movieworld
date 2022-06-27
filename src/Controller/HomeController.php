f1bb24a5fd4be3653585189a13a6d198b9fd333e
devaris <atheodor1@gmail.com>
2022-06-27 21:12:01 +0300

    minor fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,15 @@
 
 namespace App\Controller;
 
-use App\Form\MovieFormType;
 use App\Entity\Movie;
 use App\Entity\Vote;
+use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
 use App\Repository\UserRepository;
 use App\Repository\VoteRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
@@ -21,7 +18,8 @@ class HomeController extends AbstractController
     private $userRepository;
     private $voteRepository;
 
-    public function __construct(MovieRepository $movieRepository, UserRepository $userRepository, VoteRepository  $voteRepository){
+    public function __construct(MovieRepository $movieRepository, UserRepository $userRepository, VoteRepository $voteRepository)
+    {
         $this->movieRepository = $movieRepository;
         $this->userRepository = $userRepository;
         $this->voteRepository = $voteRepository;
@@ -47,7 +45,7 @@ class HomeController extends AbstractController
         $form = $this->createForm(MovieFormType::class, $movie);
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $newMovie = $form->getData();
             $user = $this->getUser();
             $newMovie->setAddedBy($user);
@@ -55,7 +53,7 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('index');
         }
 
-        return $this->render('movies/create.html.twig',[
+        return $this->render('movies/create.html.twig', [
             'form' => $form->createView()
         ]);
     }
@@ -66,7 +64,7 @@ class HomeController extends AbstractController
     public function userMovies($id): Response
     {
         $movies = $this->movieRepository->findBy(array('added_by' => $id), array('date_published' => 'DESC'));
-        $user = $this->userRepository->findOneBy(array('id' => $id),array());
+        $user = $this->userRepository->findOneBy(array('id' => $id), array());
         return $this->render('home/index.html.twig', [
             'movies' => $movies,
             'filtered_user' => $user->getName()
@@ -79,28 +77,26 @@ class HomeController extends AbstractController
     public function votePositive($movie_id): Response
     {
         $user = $this->getUser();
-        $movie = $this->movieRepository->findOneBy(array('id' => $movie_id),array());
+        $movie = $this->movieRepository->findOneBy(array('id' => $movie_id), array());
 
-        if($user && $movie && $movie->getAddedBy() != $user){
-            $vote = $this->voteRepository->findOneBy(array('movie' => $movie, 'user' => $user),array());
-            if(!$vote){
+        if ($user && $movie && $movie->getAddedBy() != $user) {
+            $vote = $this->voteRepository->findOneBy(array('movie' => $movie, 'user' => $user), array());
+            if (!$vote) {
                 $vote = new Vote();
                 $vote->setUser($user);
                 $vote->setMovie($movie);
-            }
-            if($vote->getPositive() == true){
+            } elseif ($vote->getPositive() == true) {
                 try {
                     $this->voteRepository->remove($vote);
                 } catch (\Exception $e) {
                     return $this->redirectToRoute('index');
                 }
-            } else {
-                $vote->setPositive(true);
-                try {
-                    $this->voteRepository->add($vote, true);
-                } catch (\Exception $e) {
-                    return $this->redirectToRoute('index');
-                }
+            }
+            $vote->setPositive(true);
+            try {
+                $this->voteRepository->add($vote, true);
+            } catch (\Exception $e) {
+                return $this->redirectToRoute('index');
             }
         }
 
@@ -113,28 +109,26 @@ class HomeController extends AbstractController
     public function voteNegative($movie_id): Response
     {
         $user = $this->getUser();
-        $movie = $this->movieRepository->findOneBy(array('id' => $movie_id),array());
+        $movie = $this->movieRepository->findOneBy(array('id' => $movie_id), array());
 
-        if($user && $movie && $movie->getAddedBy() != $user){
-            $vote = $this->voteRepository->findOneBy(array('movie' => $movie, 'user' => $user),array());
-            if(!$vote){
+        if ($user && $movie && $movie->getAddedBy() != $user) {
+            $vote = $this->voteRepository->findOneBy(array('movie' => $movie, 'user' => $user), array());
+            if (!$vote) {
                 $vote = new Vote();
                 $vote->setUser($user);
                 $vote->setMovie($movie);
-            }
-            if($vote->getPositive() == false){
+            } elseif ($vote->getPositive() == false) {
                 try {
                     $this->voteRepository->remove($vote);
                 } catch (\Exception $e) {
                     return $this->redirectToRoute('index');
                 }
-            } else {
-                $vote->setPositive(false);
-                try {
-                    $this->voteRepository->add($vote, true);
-                } catch (\Exception $e) {
-                    return $this->redirectToRoute('index');
-                }
+            }
+            $vote->setPositive(false);
+            try {
+                $this->voteRepository->add($vote, true);
+            } catch (\Exception $e) {
+                return $this->redirectToRoute('index');
             }
         }
 
